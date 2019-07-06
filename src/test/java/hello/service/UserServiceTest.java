90981fb788df2904a60a76e5dbb469192f53204e
Leown <leown_wang@163.com>
2019-07-06 13:03:55 +0800

    completed UserService Mokito-Test;

--- src/test/java/hello/service/UserServiceTest.java
+++ src/test/java/hello/service/UserServiceTest.java
@@ -1,16 +1,17 @@
 package hello.service;
 
+import hello.entity.User;
 import hello.mapper.UserMapper;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.mockito.junit.jupiter.MockitoSettings;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
-import static org.junit.jupiter.api.Assertions.*;
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
 
@@ -29,5 +30,31 @@ class UserServiceTest {
                 userService.save("myUser","myPassword");
 
                 Mockito.verify(mockMapper).save("myUser","myEncodedPassword");
+        }
+
+        @Test
+        public void testGetUserByUsername(){
+
+                userService.getUserByUsername("myUser");
+
+                Mockito.verify(mockMapper).findUserByname("myUser");
+
+
+        }
+        @Test
+        public void throwExceptionWhenUserNotFound(){
+
+                Assertions.assertThrows(UsernameNotFoundException.class,
+                        ()->userService.loadUserByUsername("myUser"));
+        }
+
+        @Test
+        public void returnUserDetailsWhenUserFound(){
+                Mockito.when(mockMapper.findUserByname("myUser")).
+                        thenReturn(new User(123,"myUser","myPassword"));
+
+                UserDetails userDetails =  userService.loadUserByUsername("myUser");
+                Assertions.assertEquals("myUser",userDetails.getUsername());
+                Assertions.assertEquals("myPassword",userDetails.getPassword());
         }
 }
